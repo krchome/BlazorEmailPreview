9c54ffbe7dd6f522f753391d6195b1a862f341b0
Kaushik Roy Chowdhury <devedkaushik@gmail.com>
2024-06-13 22:12:10 +1200

    Add project files.

---
+++ wwwroot/js/quillInterop.js
@@ -0,0 +1,28 @@
+﻿window.initializeQuill = function () {
+    var quill = new Quill('#editor-container', {
+        theme: 'snow'
+    });
+
+    window.quill = quill;
+}
+
+window.getQuillContent = function () {
+    return window.quill.root.innerHTML;
+}
+
+window.updatePreview = function (content) {
+    document.getElementById('preview-container').innerHTML = content;
+}
+window.saveQuillContent = function () {
+    const content = window.quill.root.innerHTML;
+    localStorage.setItem('quillContent', content);
+    alert("Content saved!");
+}
+
+window.loadQuillContent = function () {
+    const content = localStorage.getItem('quillContent');
+    if (content) {
+        window.quill.root.innerHTML = content;
+    }
+}
+
